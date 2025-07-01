7ab3f2e38f2b93350ac76088131d9a4fda2490c3
Ashutosh Maurya <ashutoshmaurya4805@gmail.com>
2025-07-01 08:50:17 +0530

    dynamicArray.c

--- dynamicArray.c
+++ dynamicArray.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 int main() {
     int n;
-    printf("Enter the size if array :");
+
+  
+    printf("Enter the size in array :");
     scanf("%d",&n);
     int *arr=(int*)malloc(n*sizeof(int));
     for(int i=0;i<=n;i++){
-        printf("enter the value at an arr[%d]index \n",i);
+        printf("enter the value at an arr[%d]index  \n \n",i);
         scanf("%d",&arr[i]);
         
     }
@@ -15,5 +17,16 @@ int main() {
     for(int i=0;i<=n;i++){
         printf("%d \n",arr[i]);
     }
+    int newsize;
+     printf("Enter the new size in array :");
+    scanf("%d",&newsize);
+    int *newarr=(int*)realloc(arr,newsize*sizeof(int));
+      for(int i=0;i<=newsize;i++){
+        printf("enter the value at an arr[%d]index \n",i);
+        scanf("%d",&newarr[i]);
+    }
+    printf("all values in newarr /n");
+       for(int i=0;i<=n;i++){
+        printf("%d \n",newarr[i]);
+    }
     return 0;
-}
