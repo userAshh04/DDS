304e26196dacd40436bb9dc0bf3f5beed4e65bb1
Ashutosh Maurya <ashutoshmaurya4805@gmail.com>
2025-07-08 08:38:55 +0530

    popFunction.cpp

---
+++ popFunction.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+#define size 5
+int stack[size];
+int top=-1;
+    void push(){
+if(top==-1){
+    cout<<"stack is underflow"
+}
+        
+    }
+int main (){
+    push(46);
+    push(88);
+    push(11);
+    push(99);
+    pop();
+    pop();
+    
+    return 0;
+}
