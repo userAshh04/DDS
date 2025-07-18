6e333e6e508863f82888e3eb89b451ad010800e1
Ashutosh Maurya <ashutoshmaurya4805@gmail.com>
2025-07-18 11:19:50 +0530

    queueDisplay.cpp

---
+++ queueDisplay.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+#define size 5
+int queue[size];
+int rear = -1, front = -1;
+
+void enqueue(int num) {
+    if (rear == size - 1) {
+        cout << "Overflow" << endl;
+    }
+    else if (rear == -1 && front == -1) {
+        front = rear = 0;
+        queue[rear] = num;
+        cout << num << " inserted" << endl;
+    }
+    else {
+        rear++;
+        queue[rear] = num;
+        cout << num << " inserted" << endl;
+    }
+}
+
+void dequeue() {
+    if (front == -1 && rear == -1) {
+        cout << "Underflow" << endl;
+    }
+    else if (front == rear) {
+        cout << queue[front] << " removed" << endl;
+        front = rear = -1;
+    }
+    else {
+        cout << queue[front] << " removed" << endl;
+        front++;
+    }
+}
+
+void display (){
+    if(front==-1 && rear==-1){
+        cout<<"underflow"<<endl;
+        
+    }
+    else{
+        for(int i=front;i<=rear;i++){
+            cout<<queue[i]<<endl;
+        }
+    }
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+
+
+display();
+    return 0;
+}
+
