e0f2c31f4f06a64195e1cc32eb88b0004ae0fdc1
Ashutosh Maurya <ashutoshmaurya4805@gmail.com>
2025-07-25 15:14:58 -1200

    pushFunction.cpp

--- pushFunction.cpp
+++ pushFunction.cpp
@@ -1,27 +1,27 @@
-
 #include <iostream>
 using namespace std;
+
 #define size 5
 int stack[size];
-int top=-1;
-    void push(int num){
-    if(top<size-1){
+int top = -1;
+
+void push(int num) {
+    if (top == size - 1) {
+        cout << "stack is overflow" << endl;
+    } else {
         top++;
-        stack[top]=num;
-        cout<<num  <<"inserted"<<endl;
-    }    
-        else{
-            cout<<"stack is overflow"<<endl;
-        }
-        
+        stack[top] = num;
+        cout << num << " inserted" << endl;
     }
-int main (){
+}
+
+int main() {
     push(46);
     push(88);
     push(11);
     push(99);
     push(17);
-    push(34);
-    
+    push(34);  // This push should trigger overflow
+
     return 0;
 }
